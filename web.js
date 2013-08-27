05e912ab4677384a30d03787648a7eba628894e8
PeetWriter <pereter92@gmail.com>
2013-08-27 14:49:54 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express = require('express')
-	, fs = require ('fs')
-	, app = express()
-	, port = process.env.PORT || 5000;
+  , fs  = require('fs')
+  , app = express()
+  , port = process.env.PORT || 5000;
+
 app.use(express.logger());
 
 var data = new Buffer(fs.readFileSync('index.html'));
 
+
 app.get('/', function(req, res){
-	res.send(data.toString('utf-8'));
+  res.send(data.toString('utf-8'));
 });
 
 app.listen(port, function(){
-	consle.log('Listening on ' + port);
+  console.log('Listenting on ' + port);
 });
